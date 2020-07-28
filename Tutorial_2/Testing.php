f0a9fa0db1795a46f733b93575e0539ff61b7c21
TudorutDragos <dragos..tudorut@yahoo.com>
2020-07-28 17:42:31 +0300

    still playing with branches

---
+++ Tutorial_2/Testing.php
@@ -0,0 +1,40 @@
+<?php
+    //Integers
+    /*$regInt =-1234;
+
+    $octNum =01234;
+
+    $hexNum = 0xABC;
+
+    $binaryNum = 0b1000;
+
+    var_dump($regInt);
+
+    var_dump($octNum);
+
+    var_dump($hexNum);
+
+    var_dump($binaryNum);*/
+
+
+    //floating
+    /*$float = 1.234;
+
+    $scientific = 0.1234E4;
+
+    $scientific = 1234E-4;
+
+    var_dump($scientific);*/
+
+    /*define('CHECK_CONSTANT','constanta');
+
+    $intVar=123;
+    $stringVar='sapoj';
+    $boolVar=false;
+    $floatVar=12.13;
+
+    echo is_int($floatVar);
+    echo is_string($intVar);
+
+    echo is_bool($boolVar);*/
+?>
